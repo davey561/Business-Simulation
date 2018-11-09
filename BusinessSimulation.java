f520648d092ff6a62329cdf34a0dba617ac9261e
22sdw2 <22sdw2@tcl216-6.cs.williams.edu>
2018-11-09 16:55:10 -0500

    switching to davey's mac lab account yeet yeet

--- BusinessSimulation.java
+++ BusinessSimulation.java
@@ -6,61 +6,41 @@ import structure5.*;
 
 
 /**
-
- * TODO: class description here.
-
+ * BusinessSimulation provides the overarching methods for simulating the lines of a store as customers arrive and leave and tellers attend to customers and wait for new ones.
+ * The two class's two children are simulate two different store-policies of how lines should form: MultiQueue and OneQueue
+ * @author DaveyMorse
+ * @author Stephen Willis
  */
 
 public abstract class BusinessSimulation {
-
-
+	//Declarations
 	/* sequence of customers, prioritized by randomly-generated event time */
-
 	protected PriorityQueue<Customer> eventQueue;
-
-
 	/* series of service points where customers queue and are served */
-
 	protected Vector<Queue<Customer>> servicePoints;
-
-
 	/* current time step in the simulation */
-
 	protected int time;
-
-
 	/* seed for Random() so that simulation is repeatable */
-
 	protected int seed;
-
-
 	/* Used to bound the range of service times that Customers require */
 	public static final int MIN_SERVICE_TIME = 5; //TODO: set appropraitely
 	public static final int MAX_SERVICE_TIME = 20; //TODO: set appropriately
 
 	/**
-
 	 * Creates a BusinessSimulation.
-
 	 * @post the step() function may be called.
-
 	 *
-
 	 * @param numCustomers number of random customers that appear in the simulation
-
 	 * @param numSerivicePoints number of tellers in this simulation
-
 	 * @param maxEventStart latest timeStep that a Customer may appear in the simulation
-
 	 * @param seed used to seed a Random() so that simulation is repeatable.
-
 	 */
 
 	public BusinessSimulation(int numCustomers, int numServicePoints,
 				  int maxEventStart, int seed) {
 						this.servicePoints = new Vector<Queue<Customer>>();
 						for(int i=0; i<numServicePoints; i++) {
-							this.servicePoints.add(new QueueList());
+							this.servicePoints.add(new QueueList<Customer>());
 						}
 						this.eventQueue=generateCustomerSequence(numCustomers, maxEventStart, seed);
 						this.time=0;
@@ -68,17 +48,11 @@ public abstract class BusinessSimulation {
 				}
 
 	/**
-
 	 * Generates a sequence of Customer objects, stored in a PriorityQueue.
-
 	 * Customer priority is determined by arrival time
-
 	 * @param numCustomers number of customers to simulate
-
 	 * @param maxEventStart maximum timeStep that a customer arrives
-
 	 *      in @eventQueue
-
 	 * @param seed use Random(seed) to make customer sequence repeatable
 
 	 * @pre
@@ -102,8 +76,9 @@ public abstract class BusinessSimulation {
 						for(int i=0; i<numCustomers; i++) {
 
 							line.add(new Customer(
-								(int)(rnd.nextDouble() * maxEventStart)),
-								(int)(rnd.nextDouble() * (MAX_SERVICE_TIME - MIN_SERVICE_TIME)) + MIN_SERVICE_TIME));
+								(int)(rnd.nextDouble() * maxEventStart),
+								(int)(rnd.nextDouble() * (MAX_SERVICE_TIME - MIN_SERVICE_TIME)) + MIN_SERVICE_TIME
+							));
 						}
 
 						return line;
